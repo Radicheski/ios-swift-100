34000c50dcd0762f76ce6c1ad4f5b0753c054421
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-05-28 13:29:39 -0300

    Finished Project 1, part one

---
+++ Project01/Project01/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project01
+//
+//  Created by Erik Radicheski da Silva on 28/05/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let fileManagegr = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManagegr.contentsOfDirectory(atPath: path)
+
+        items.filter { $0.hasPrefix("nssl") }
+            .forEach { pictures.append($0) }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+
+        var conf = cell.defaultContentConfiguration()
+        conf.text = pictures[indexPath.row]
+        cell.contentConfiguration = conf
+
+        return cell
+    }
+
+}
