d4a050144c06c656b7144475f3a134a26ba66cdc
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-06-19 09:40:20 -0300

    Finished Project 12, part one

---
+++ Project12a/Project12a/PersonCell.swift
@@ -0,0 +1,21 @@
+//
+//  PersonCell.swift
+//  Project12a
+//
+//  Created by Erik Radicheski da Silva on 12/06/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        imageView.image = nil
+        nameLabel.text = nil
+    }
+}
