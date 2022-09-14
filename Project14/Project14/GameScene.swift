4431a38c9a2df608e817f0a20be0fce475abb9e3
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-09-14 17:25:00 -0300

    Finished Project 14, part two

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -19,6 +19,7 @@ class GameScene: SKScene {
     }
     
     var popupTime = 0.85
+    var numRounds = 0
 
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
@@ -45,7 +46,29 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
         
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
+            
+            if !whackSlot.isVisible { continue }
+            if whackSlot.isHit { continue }
+            
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                score -= 5
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy" {
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+                score += 1
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
     }
     
     func createSlot(at position: CGPoint) {
@@ -56,6 +79,27 @@ class GameScene: SKScene {
     }
     
     func createEnemy() {
+        numRounds += 1
+        
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            
+            let score = SKLabelNode(fontNamed: "Chalkduster")
+            score.horizontalAlignmentMode = .center
+            score.position = CGPoint(x: 512, y: 258)
+            score.text = "Your score: \(self.score)"
+            addChild(score)
+            
+            return
+        }
+        
         popupTime *= 0.991
         
         slots.shuffle()
